fb683ac60a63cb62c08feb20bda681d22271fcbf
_xoxo_ <aanch.rai@gmail.com>
2018-07-22 13:47:59 +0530

    Create Account.java

---
+++ AssignmentWeek1-Q2/bank/Account.java
@@ -0,0 +1,12 @@
+package bank;
+
+public interface Account
+{
+	public  String Savings = null;
+	public  String  Fixed = null;
+    public  String PersonalLoan = null;
+	public  String HousingLoan = null;
+	public void creatAcc();
+}
+
+

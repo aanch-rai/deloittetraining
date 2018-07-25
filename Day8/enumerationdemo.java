3f26b7e7d37c9f6d3bcc564055ec90152bdd59e6
_xoxo_ <aanch.rai@gmail.com>
2018-07-25 17:00:26 +0530

    Create enumerationdemo.java

---
+++ Day8/enumerationdemo.java
@@ -0,0 +1,27 @@
+//Enumeration is primarily used to iterate similar to iterator
+package Day8;
+
+import java.util.Enumeration;
+
+//import java.util.*;
+
+
+
+public class enumerationdemo
+{
+	public static void main(String[] args)
+	{
+		char x[]= {'a','c'};
+		for(char y:x)
+		{System.out.println(y);}
+		
+	 
+	/*	Properties p = System.getProperties();
+		Enumeration<Object> e= p.elements();
+		while(e.hasMoreElements())
+		{
+			System.out.println("The value is :"+e.nextElement());
+		}*/
+	}
+
+}
